9c88b457339948a69a92ebdfe2fbb66f4a5254bc
iidzim9 <52450700+iidzim9@users.noreply.github.com>
2021-06-24 17:36:14 +0100

    sort_big_stack

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: iidzim <iidzim@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/27 14:24:43 by iidzim            #+#    #+#              #
-#    Updated: 2021/06/23 18:35:42 by iidzim           ###   ########.fr        #
+#    Updated: 2021/06/24 11:21:19 by iidzim           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -37,10 +37,10 @@ RM				=	rm -f
 all:			$(NAME)
 
 $(NAME1):		$(OBJS)
- 				@$(CC) -o $(NAME1) $(SRCS) $(FLAGS)
+				$(CC) -o $(NAME1) $(SRCS) $(FLAGS)
 
 bonus :			$(OBJB)
- 				@$(CC) -o $(NAME2) $(SRCB) $(FLAGS)
+				@$(CC) -o $(NAME2) $(SRCB) $(FLAGS)
 
 clean:
 				@$(RM) $(OBJS) $(OBJB)
--- push_swap_d/push_swap.c
+++ push_swap_d/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: iidzim <iidzim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 18:51:15 by iidzim            #+#    #+#             */
-/*   Updated: 2021/06/23 18:55:37 by iidzim           ###   ########.fr       */
+/*   Updated: 2021/06/24 15:46:27 by iidzim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,7 @@ int	operations_3num(t_all *x, int max, int top, int middle)
 	if (max == top)
 		rot(x, 'a', 1);
 	if (max == middle)
-	{
-		swap(x, 'a', 1);
-		rot(x, 'a', 1);
-	}
+		reverse_rot(x, 'a', 1);
 	top = x->a->value;
 	middle = x->a->next->value;
 	if (top > middle)
@@ -80,7 +77,7 @@ void	push_min_to_stack(t_all *x, int min, char c)
 	}
 	else
 	{
-		while ((cpt--) - x->size_a / 2 > 0)
+		while ((--cpt) - x->size_a / 2> 0)
 			reverse_rot(x, 'a', 1);
 	}
 	push(x, c, 1);
@@ -91,9 +88,12 @@ int	sort_5num(t_all *x)
 	int	min1;
 	int	min2;
 
-	min1 = find_max_min(x->a, 2);
-	push_min_to_stack(x, min1, 'b');
-	min2 = find_max_min(x->a, 2);
+	if (x->size_a == 5)
+	{
+		min1 = find_max_min(x->a, 2);
+		push_min_to_stack(x, min1, 'b');
+	}
+	min2 = find_max_min(x->a, 2);;
 	push_min_to_stack(x, min2, 'b');
 	sort_3num(x);
 	push(x, 'a', 1);
@@ -101,7 +101,7 @@ int	sort_5num(t_all *x)
 	return (x->moves);
 }
 
-void	sort_stack(t_all *x)
+void	sort_small_stack(t_all *x)
 {
 	if (x->size_a == 2)
 	{
@@ -111,24 +111,45 @@ void	sort_stack(t_all *x)
 	else if (x->size_a == 3)
 		sort_3num(x);
 	else
-		sort_5num(x);
-	printf("sorted\n");
-	print_list(x->a);
+		x->moves = sort_5num(x);
+	printf("moves >>>>>>%d\n", x->moves);
 }
 
-// void	sort_stack(t_all *x)
-// {
-// 	if (x->size_a <= 5)
-// 		sort_small_stack(x);
-// 	else
-// 		sort_big_stack(x);
-// 	printf("sorted\n");
-// 	print_list(x->a);
-// }
+int	sort_big_stack(t_all *x)
+{
+	int *dup_stack_a;
+	t_list *temp;
+	int	i;
+
+	dup_stack_a = malloc(sizeof(int) * (x->size_a + 1));
+	if (!dup_stack_a)
+		return (0);
+	temp = x->a;
+	i = 0;
+	while (temp)
+	{
+		dup_stack_a[i++] = temp->value;
+		temp = temp->next;
+	}
+	dup_stack_a[i] = '\0';
+	
+	
+	return (0);
+}
 
 int	main(int argc, char **argv)
 {
 	t_all	x;
+
+	// int num = 3;
+	// int maxbits = 0;
+	// while((num >> (maxbits - 1)) != 0)
+	// 	maxbits++;
+	// if (((num>>maxbits)&1) == 1)
+	// 	printf("1\n");
+	// printf("max_bits = %d\n", maxbits);
+	// // printf("num = %d\n", num>>3);
+	// return (0);
 
 	if (argc >= 2)
 	{
@@ -136,7 +157,14 @@ int	main(int argc, char **argv)
 		if (sorted(x.a))
 			printf("OK\n");
 		else
-			sort_stack(&x);
+		{
+			if (x.size_a <= 5)
+				sort_small_stack(&x);
+			else
+				sort_big_stack(&x);
+			printf("sorted\n");
+			print_list(x.a);
+		}
 		ft_freelst(x.a);
 		ft_freelst(x.b);
 		return (0);
@@ -150,3 +178,35 @@ int	main(int argc, char **argv)
 //The time complexity of quicksort is dependent upon the partition and
 //how sorted the list already is.
 //check if the list is already sorted
+
+
+/*
+{
+	int size = a.size();
+	int max_num = size - 1; // the biggest number in a is stack_size - 1
+	int max_bits = 0; // how many bits for max_num 
+	while ((max_num >> max_bits) != 0) ++max_bits;
+	for (int i = 0 ; i < max_bits ; ++i) // repeat for max_bits times
+	{
+	    for(int j = 0 ; j < size ; ++j)
+	    {
+	        int num = a.top(); // top number of A
+	        if ((num >> i)&1 == 1) ra(); 
+	        ///? if the (i + 1)-th bit is 1, leave in stack a
+	        else pb();
+	        ///? otherwise push to stack b
+	    }
+	    ///? put into boxes done
+	    while (!b.empty()) pa(); // while stack b is not empty, do pa
+	
+	    ///? connect numbers done
+	}
+}
+*/
+
+
+//ToDo 
+// duplicate stack a -> temp 
+// bubble sort stack a 
+// indexation -> stack a
+// radix sort
